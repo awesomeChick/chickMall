fed0ad3acefa47063d3a09d628988fdb6ac37136
@chenrongji <chenrj@gillion.com.cn>
2021-11-28 16:27:50 +0800

    项目结构初始化

---
+++ gulimall-proudct/src/test/java/com/atguigu/gulimall/proudct/GulimallProudctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.proudct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProudctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
